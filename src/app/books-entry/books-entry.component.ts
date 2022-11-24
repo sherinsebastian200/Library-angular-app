a16653d13d59b2ebec3fdfec201cbfb8520d5d35
sherin200 <sherinsebastian21>
2022-11-24 19:04:30 +0530

    entry"

---
+++ src/app/books-entry/books-entry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-entry',
+  templateUrl: './books-entry.component.html',
+  styleUrls: ['./books-entry.component.css']
+})
+export class BooksEntryComponent {
+
+}
